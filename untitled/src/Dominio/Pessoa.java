7a07a6f285e4866b03b5d2f976532e074edcd543
Leonardo Schio <leonardoschio92@gmail.com>
2025-03-28 20:23:24 -0300

    Foi criada uma classe Pessoa e adicianado atributos nela e uma calsse Pessoatest

---
+++ untitled/src/Dominio/Pessoa.java
@@ -0,0 +1,7 @@
+package Dominio;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+    public double altura;
+}
---
+++ untitled/src/Test/PessoaTest.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+
+    }
+}
