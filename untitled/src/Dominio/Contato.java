d4515313eaa99842864709bd5d3d5d239b3fd49c
Leonardo Schio <leonardoschio92@gmail.com>
2025-03-29 18:08:21 -0300

    Foi criado os getters and setters

--- untitled/src/Dominio/Contato.java
+++ untitled/src/Dominio/Contato.java
@@ -8,4 +8,20 @@ public class Contato {
         this.nome = nome;
         this.numero = numero;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
