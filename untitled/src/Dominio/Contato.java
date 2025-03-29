f4f40d7da860c8de83298e3d3e57ed91951139c3
Leonardo Schio <leonardoschio92@gmail.com>
2025-03-29 17:37:50 -0300

    Criamos um construtor para a classe Contato

--- untitled/src/Dominio/Contato.java
+++ untitled/src/Dominio/Contato.java
@@ -1,5 +1,11 @@
 package Dominio;
 
 public class Contato {
-    
+    public String nome;
+    public int numero;
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
 }
